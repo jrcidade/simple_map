4543857ac76080e9a17888da6b488b0f4292f1f4
jrcidade <jrcidade@gmail.com>
2023-11-30 08:47:34 -0400

    update location

--- static/js/script.js
+++ static/js/script.js
@@ -8,4 +8,28 @@ var lng = -61.1938;
 
 var map = L.map("map", config).setView([lat, lng], zoom);
 
-L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}.png",).addTo(map);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png",).addTo(map);
+
+map
+  .locate({
+    setView: true,
+    enableHighAccuracy: true,
+  })
+  .on("locationfound", (e) => {
+    console.log(e);
+    const marker = L.marker([e.latitude, e.longitude]).bindPopup(
+      "Você está aqui !"
+    );
+    const circle = L.circle([e.latitude, e.longitude], e.accuracy / 2, {
+      weight: 2,
+      color: "red",
+      fillColor: "red",
+      fillOpacity: 0.1,
+    });
+    map.addLayer(marker);
+    map.addLayer(circle);
+  })
+  .on("locationerror", (e) => {
+    console.log(e);
+    alert("Location access denied.");
+  });
